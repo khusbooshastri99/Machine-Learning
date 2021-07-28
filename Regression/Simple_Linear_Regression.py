8c3f93fb91e87989d5a287ab2a0e29ea46b47131
Khushboo Shastri <80696265+khusbooshastri99@users.noreply.github.com>
2021-07-28 10:12:07 +0530

    initial_commit

--- Regression/Simple_Linear_Regression.py
+++ Regression/Simple_Linear_Regression.py
@@ -1,31 +1,31 @@
-# Importing the libraries
+#Importing the libraries
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#Importing the dataset
 
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
+#Splitting the dataset into the Training set and Test set
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Training the Simple Linear Regression model on the Training set
+#Training the Simple Linear Regression model on the Training set
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
+#Predicting the Test set results
 
 y_pred = regressor.predict(X_test)
 
-# Visualising the Training set results
+#Visualising the Training set results
 
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
@@ -34,7 +34,7 @@ plt.xlabel('Years of Experince')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Test set results
+#Visualising the Test set results
 
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
